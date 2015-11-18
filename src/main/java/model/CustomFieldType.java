5140d5a8466c233fe836369655333be73059fc95
IIpu3pak <IIpu3pak7@gmail.com>
2015-11-18 12:06:10 +0200

    (data base model)

---
+++ src/main/java/model/CustomFieldType.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.io.Serializable;
+
+
+public class CustomFieldType implements Serializable {
+
+    private int id;
+    private String name;
+    private Class type;
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Class getType() {
+        return type;
+    }
+
+    public void setType(Class type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomFieldType)) return false;
+
+        CustomFieldType that = (CustomFieldType) o;
+
+        if (getId() != that.getId()) return false;
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+        return !(getType() != null ? !getType().equals(that.getType()) : that.getType() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getType() != null ? getType().hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/model/CustomFieldValue.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.Serializable;
+
+
+public class CustomFieldValue implements Serializable {
+
+    private int id;
+    private Object value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomFieldValue)) return false;
+
+        CustomFieldValue that = (CustomFieldValue) o;
+
+        if (getId() != that.getId()) return false;
+        return !(getValue() != null ? !getValue().equals(that.getValue()) : that.getValue() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
+        return result;
+    }
+}
